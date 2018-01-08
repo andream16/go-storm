18942f07049e140d78a2d0f0e3866aa27215f0bf
andream16 <andrea.medda.uni@gmail.com>
2018-01-08 14:47:08 +0100

    Removing unnecessary Enumeration for Forecast's names.

--- psql/enum/enum.go
+++ psql/enum/enum.go
@@ -1,6 +1,5 @@
 package enum
 
 var ENUMERATIONS = map[string]string {
-	"Forecast": "CREATE TYPE forecast_name AS ENUM ('ATCR','ATR','ACR','ATC','AT','AC','AR')",
 	"Currency":"CREATE TYPE currency_name AS ENUM ('EURO', 'DOLLAR')",
 }
